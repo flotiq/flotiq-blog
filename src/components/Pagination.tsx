244435495dbeae76260903e983f21fde33ab8b6d
Kinga Knapik <kinga.knapik@codewave.pl>
2020-11-24 09:02:55 +0100

    working pagination links

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -53,8 +53,8 @@ const navCss = css`
 const Pagination: React.FunctionComponent<PaginationProps> = ({ currentPage, numPages }) => {
   const isFirst = currentPage === 1;
   const isLast = currentPage === numPages;
-  const prevPage = currentPage - 1 === 1 ? '/' : (currentPage - 1).toString();
-  const nextPage = (currentPage + 1).toString();
+  const prevPage = currentPage - 1 === 1 ? '/' : `/${(currentPage - 1).toString()}`;
+  const nextPage = `/${(currentPage + 1).toString()}`;
 
   return (
     <nav css={navCss}>
