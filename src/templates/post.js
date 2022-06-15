2e9f1c5f4f6e7be2256e75d2633e067afb69ee7a
veto221 <=>
2022-06-15 08:51:53 +0200

    ignore origin

--- src/templates/post.js
+++ src/templates/post.js
@@ -75,7 +75,7 @@ const PostPage = ({ data, pageContext }) => {
                 {(post.headerImage) && (
                     <meta
                         property="og:image"
-                        content={(origin ?? data.site.siteMetadata.siteUrl) + post.headerImage[0].localFile.publicURL}
+                        content={data.site.siteMetadata.siteUrl + post.headerImage[0].localFile.publicURL}
                     />
                 )}
                 <meta property="article:published_time" content={post.publish_date} />
@@ -94,7 +94,7 @@ const PostPage = ({ data, pageContext }) => {
                 {(post.headerImage) && (
                     <meta
                         name="twitter:image"
-                        content={(origin ?? data.site.siteMetadata.siteUrl) + post.headerImage[0].localFile.publicURL}
+                        content={data.site.siteMetadata.siteUrl + post.headerImage[0].localFile.publicURL}
                     />
                 )}
                 <meta name="twitter:label1" content="Written by" />
