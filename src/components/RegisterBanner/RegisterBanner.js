abc7a3631c4f4f4e7b0109071c2a3f075093d565
Krzysztof Brylski <krzysztof.brylski@codewave.pl>
2023-10-05 16:46:50 +0200

    small refactor, implemented doc

--- src/components/RegisterBanner/RegisterBanner.js
+++ src/components/RegisterBanner/RegisterBanner.js
@@ -1,31 +1,31 @@
-import React, {useCallback} from "react";
-import Button from "../Button/Button";
+import React, { useCallback } from 'react';
+import Button from '../Button/Button';
 import registerBannerIcon from '../../assets/register-banner-icon.svg';
 
 export const RegisterBanner = () => {
-
     const Url = 'https://editor.flotiq.com';
 
-    const handleButton = useCallback(() => window.open(Url), [Url])
+    const handleButton = useCallback(() => window.open(Url), [Url]);
 
+    return (
+        <div className="register-banner d-flex justify-content-between items-center w-full">
+            <div>
+                <div className={'mb-5'}>
+                    <h2>Discover how to build </h2>
+                    <h2>Better products with Flotiq</h2>
+                </div>
 
-    return <div
-        className="register-banner d-flex justify-content-between items-center w-full"
-    >
-        <div>
-            <div className={"mb-5"}>
-                <h2>Discover how to build </h2>
-                <h2>Better products with Flotiq</h2>
+                <Button
+                    click={handleButton}
+                    additionalClasses={['p-2', 'px-4']}
+                >
+                    Start for Free
+                </Button>
             </div>
 
-            <Button click={handleButton} additionalClasses={['p-2', 'px-4']}>
-                Start for Free
-            </Button>
+            <img src={registerBannerIcon} alt="Flotiq" />
         </div>
-
-        <img src={registerBannerIcon} alt="Flotiq"/>
-    </div>
-}
-
+    );
+};
 
 export default RegisterBanner;
--- src/helpers/blocksEmbed.js
+++ src/helpers/blocksEmbed.js
@@ -1,23 +1,30 @@
-import RegisterBanner from "../components/RegisterBanner/RegisterBanner";
-import React from "react";
+import RegisterBanner from '../components/RegisterBanner/RegisterBanner';
+import React from 'react';
 
-
+/**
+ * map with available shortcodes and their components
+ * @type {{register_banner: JSX.Element}}
+ */
 const blocksMap = {
-    ['register_banner']: <RegisterBanner/>,
-}
+    ['register_banner']: <RegisterBanner />,
+};
 
-
+/**
+ *
+ * @param data array with Flotiq block editor content
+ * @returns {*}
+ */
 export const blocksEmbed = (data) => {
     for (const property in data) {
         Object.keys(blocksMap).map((code) => {
             if (
-                (data[property].data.text instanceof String
-                    || typeof data[property].data.text === 'string')
-                && data[property].data.text.indexOf(`[[${code}]]`) > -1
+                (data[property].data.text instanceof String ||
+                    typeof data[property].data.text === 'string') &&
+                data[property].data.text.indexOf(`[[${code}]]`) > -1
             ) {
                 data[property].data.text = blocksMap[code];
             }
-        })
+        });
     }
     return data;
 };
